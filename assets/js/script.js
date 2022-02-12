92c0c0ffbee2ab395f6e2338854f71fa64dcc758
shahery <shaheryar924@gmail.com>
2022-02-12 11:37:25 +0000

    Add final changes to the project

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,7 +140,7 @@ function displayMulyiplyQuestion(operand1, operand2){
 
 function displayDivisionQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1*operand2>operand2 ? operand1: operand2;
-	document.getElementById("operand2").textContent = operand1*operand2>operand1? operand1: operand2;
+	document.getElementById("operand2").textContent = operand1*operand2>operand2 ? operand1: operand2;
 	document.getElementById("operator").textContent = "/";
 
 }
